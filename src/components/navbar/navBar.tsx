ee1a9e64ee0424f8f02c1ebfa07a738a7e8b21d4
mln-i <mln-i@hotmail.com>
2019-11-18 11:28:55 +0100

    Add react-native-elements

--- src/components/navbar/navBar.tsx
+++ src/components/navbar/navBar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
+import { Header, ListItem } from 'react-native-elements';
 
 export default class navBar extends React.Component {
     
