5b86358a3ada4d3fee41f970145d3b6f7ab21f72
Martijn Vegter <martijn@martijnvegter.com>
2019-11-21 14:43:03 +0100

    Fixed some ESLint warnings

--- src/styles/mixins.tsx
+++ src/styles/mixins.tsx
@@ -4,14 +4,14 @@ import { Dimensions, PixelRatio } from 'react-native';
 const DEVICE_WIDTH = Dimensions.get('window').width;
 const DEVICE_HEIGHT = Dimensions.get('window').height;
 
-const widthPercentageToDP = (widthPercent: string) => {
+const widthPercentageToDP = (widthPercent: string): number => {
 
     // Convert string input to decimal number
     const elemWidth = parseFloat(widthPercent);
     return PixelRatio.roundToNearestPixel(DEVICE_WIDTH * elemWidth / 100);
 };
 
-const heightPercentageToDP = (heightPercent: string) => {
+const heightPercentageToDP = (heightPercent: string): number => {
 
     // Convert string input to decimal number
     const elemHeight = parseFloat(heightPercent);
@@ -25,9 +25,9 @@ export {
 
 const guidelineBaseWidth = 375;
 
-export const scaleSize = (size: number) => (DEVICE_WIDTH / guidelineBaseWidth) * size;
+export const scaleSize = (size: number): number => (DEVICE_WIDTH / guidelineBaseWidth) * size;
 
-export const scaleFont = (size: number) => size * PixelRatio.getFontScale();
+export const scaleFont = (size: number): number => size * PixelRatio.getFontScale();
 
 function dimensions(top: number, right = top, bottom = top, left = right, property: string) {
     const styles = {
@@ -47,8 +47,7 @@ export function padding(top: number, right: number, bottom: number, left: number
     return dimensions(top, right, bottom, left, 'padding');
 }
 
-export function boxShadow(color: string, offset = { height: 2, width: 2 },
-    radius = 8, opacity = 0.2) {
+export function boxShadow(color: string, offset = { height: 2, width: 2 }, radius = 8, opacity = 0.2) {
     return {
         shadowColor: color,
         shadowOffset: offset,
