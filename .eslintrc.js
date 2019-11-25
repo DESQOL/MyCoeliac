b5282ba40a887de8a15ed4887dd322279e6c3b3e
Martijn Vegter <martijn@martijnvegter.com>
2019-11-25 12:07:41 +0100

    chore(style): updated eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,9 @@ module.exports = {
     rules: {
         "@typescript-eslint/semi": ["error"],
         "eol-last": ["error", "always"],
+        "indent": ["error", 4, { "MemberExpression": 1 }],
         "object-curly-spacing": ["error", "always"],
+        "prefer-arrow-callback": "error",
         "quotes": ["error", "single", "avoid-escape"],
         "semi": "off",
     },
