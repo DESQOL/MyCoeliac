e417915e70a080e67caeb94ffc6a9dcb5d41188f
mln-i <mln-i@hotmail.com>
2019-11-29 15:12:05 +0100

    Adjust style of component Recipe Card

--- src/components/recipe_card/recipeCard.tsx
+++ src/components/recipe_card/recipeCard.tsx
@@ -3,6 +3,9 @@ import { Text, View, StyleSheet, Image, FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/Entypo';
 
+import { GrayLighter, GrayLightest } from '../../styles/config/colors';
+import { FONT_SIZE_14, FONT_SIZE_17, FONT_SIZE_20 } from '../../styles/config/font';
+
 interface AppProps {
     recipeProps: any;
 }
@@ -10,15 +13,62 @@ interface AppProps {
 const starRating: any = [];
 
 const styles = StyleSheet.create({
+    description: {
+        fontSize: FONT_SIZE_14,
+    },
+
+    duration: {
+        position: 'absolute',
+    },
+
     logo: {
+        alignSelf: 'center',
         height: 200,
-        width: 200,
+        width: '100%',
     },
 
     ratingContainer: {
+        alignSelf: 'flex-end',
         display: 'flex',
         flexDirection: 'row',
-    }
+        position: 'absolute',
+    },
+
+    recipeCardContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        paddingLeft: 28,
+        paddingRight: 28,
+    },
+
+    recipeData: {
+        marginBottom: 25,
+    },
+
+    recipeListItem: {
+        backgroundColor: GrayLightest,
+        padding: 5,
+    },
+
+    recipeListItemContent: {
+        fontSize: 14,
+        marginLeft: 8,
+        marginRight: 8,
+    },
+
+    subtitle: {
+        fontSize: FONT_SIZE_17,
+        marginBottom: 10,
+        marginTop: 10,
+    },
+
+    title: {
+        fontSize: FONT_SIZE_20,
+        marginBottom: 10,
+        marginTop: 10,
+    },
+
 });
 
 function renderRating(rating: number) {
@@ -33,30 +83,49 @@ function renderRating(rating: number) {
     }
 }
 
+function renderSeparator() {
+    return (
+        <View
+            style={{
+                height: 1,
+                backgroundColor: GrayLighter,
+            }}
+        />
+    );
+}
+
 export default function RecipeCard(props: AppProps): JSX.Element {
 
     renderRating(props.recipeProps[0].rating || 0);
 
-    return (<View>
-        <Text>{props.recipeProps[0].title}</Text>
+    return (<View style={styles.recipeCardContainer}>
+        <Text style={styles.title}>{props.recipeProps[0].title}</Text>
 
         <Image style={styles.logo} source={{ uri: props.recipeProps[0].image || '' }}/>
 
-        <View style={styles.ratingContainer}>{starRating}</View>
+        <View style={styles.recipeData}>
+            <Text style={styles.duration}>{props.recipeProps[0].duration}</Text>
+
+            <View style={styles.ratingContainer}>{starRating}</View>
+        </View>
 
         <Text>{props.recipeProps[0].description}</Text>
 
-        <br/>
-        <Text>Ingredients</Text>
-        <br/>
+        <Text style={styles.subtitle}>Ingredients</Text>
         <FlatList
             data={props.recipeProps[0].ingredients}
-            renderItem={({ item }: { item: any }) => <ListItem title={item.amount + ' ' + item.name}/>}
+            renderItem={({ item }: { item: any }) => <ListItem
+                // key={item.id}
+                containerStyle={styles.recipeListItem}
+                titleStyle={styles.recipeListItemContent}
+                title={item.amount + ' ' + item.name}/>}
+            ItemSeparatorComponent={renderSeparator}
+            keyExtractor={(item, index) => item.id.toString()}
         />
 
-        <br/>
-        <Text>Instructions</Text>
-        <br/>
-        <Text>{props.recipeProps[0].instructions}</Text>
+        <Text style={styles.subtitle}>Instructions</Text>
+        <Text style={styles.description}>{props.recipeProps[0].instructions}</Text>
     </View>);
 }
+
+
