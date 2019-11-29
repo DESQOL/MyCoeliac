793e7488b920c19e6340931a0a2ac8e517b2ce64
Jordy <jordy.boelhouwer@hva.nl>
2019-11-29 18:55:02 +0100

    [Fix] ESlint fix

--- src/screens/home/recipeScreen.tsx
+++ src/screens/home/recipeScreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { View, Text, StyleSheet, FlatList, SafeAreaView } from 'react-native';
-import { ListItem, SearchBar, Button } from 'react-native-elements';
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
+import { SearchBar, Button } from 'react-native-elements';
 
 import RecipeList from '../../components/recipe_list/recipeList';
 import NavBar from '../../components/navbar/navBar';
@@ -15,85 +15,85 @@ interface RecipeScreenProps {
 }
 
 const dummyList = [
-  {
-    id: 1,
-    image:
+    {
+        id: 1,
+        image:
       'https://www.simplyrecipes.com/wp-content/uploads/2014/08/banana-bread-vertical-c-1200.jpg',
-    title: 'Banana bread',
-    description: '...',
-    rating: 4,
-  },
-  {
-    id: 2,
-    image: null,
-    title: 'Chocolate cake',
-    description: '...',
-    rating: 3,
-  },
+        title: 'Banana bread',
+        description: '...',
+        rating: 4,
+    },
+    {
+        id: 2,
+        image: null,
+        title: 'Chocolate cake',
+        description: '...',
+        rating: 3,
+    },
 ];
 
 const styles = StyleSheet.create({
-  title: {
-    fontSize: 25,
-  },
+    title: {
+        fontSize: 25,
+    },
 });
 
 export default class RecipeScreen extends React.Component<
   RecipeScreenProps,
   RecipeScreenState
 > {
-  constructor(props: RecipeScreenProps) {
-    super(props);
-    console.log('test');
-  }
+    constructor(props: RecipeScreenProps) {
+        super(props);
+        console.log('test');
+    }
 
-  componentDidMount() {
-    this.setState({
-      search: '',
-      recipes: dummyList,
-    });
-  }
+    componentDidMount() {
+        this.setState({
+            search: '',
+            recipes: dummyList,
+        });
+    }
 
   public readonly state: RecipeScreenState = {
-    search: '',
-    recipes: dummyList,
+      search: '',
+      recipes: dummyList,
   };
 
   updateSearch = (search: any) => {
-    if (search === '') {
-      this.setState({
-        recipes: dummyList,
-        search: search,
+      if (search === '') {
+          this.setState({
+              recipes: dummyList,
+              search: search,
+          });
+          return;
+      }
+      const newData = this.state.recipes.filter((recipe: any) => {
+          const recipeData = recipe.title.toUpperCase();
+          const searchData = search.toUpperCase();
+          return recipeData.indexOf(searchData) > -1;
       });
-      return;
-    }
-    const newData = this.state.recipes.filter(function(recipe: any) {
-      const recipeData = recipe.title.toUpperCase();
-      const searchData = search.toUpperCase();
-      return recipeData.indexOf(searchData) > -1;
-    });
-    this.setState({
-      recipes: newData,
-      search: search,
-    });
+      this.setState({
+          recipes: newData,
+          search: search,
+      });
   };
 
   render(): JSX.Element {
-    return (
-      <SafeAreaView>
-        <NavBar />
-        <View>
-          <Text style={styles.title}>Recipes</Text>
-          <SearchBar
-            placeholder="Search recipes"
-            onChangeText={search => this.updateSearch(search)}
-            value={this.state.search}
-            lightTheme={true}
-          />
-          <RecipeList recipes={this.state.recipes} />
-          <Button title="Add new recipe" />
-        </View>
-      </SafeAreaView>
-    );
+      return (
+          <SafeAreaView>
+              <NavBar />
+              <View>
+                  <Text style={styles.title}>Recipes</Text>
+                  <SearchBar
+                      placeholder="Search recipes"
+                      onChangeText={search => this.updateSearch(search)}
+                      value={this.state.search}
+                      lightTheme={true}
+                  />
+                  <RecipeList recipes={this.state.recipes} />
+                  <Button title="Add new recipe" />
+              </View>
+          </SafeAreaView>
+      );
   }
 }
