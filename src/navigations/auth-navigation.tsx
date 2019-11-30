86faaefaedb0a847d3c7ca39b791f4eade6b4b2d
walorda <walorda@gmail.com>
2019-11-30 18:06:17 +0100

    added registration screen

--- src/navigations/auth-navigation.tsx
+++ src/navigations/auth-navigation.tsx
@@ -1,15 +1,17 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import LoginScreen from '../screens/login/loginScreen';
+import RegisterScreen from '../screens/registration/registrationScreen';
 import { HeaderMode } from 'react-navigation-stack/lib/typescript/types';
 
 const AuthNavigatorConfig = {
-  initialRouteName: 'Login',
+  initialRouteName: 'Register',
   header: null,
   headerMode: 'none' as HeaderMode
 };
 
 const RouteConfigs = {
     Login: LoginScreen,
+    Register: RegisterScreen,
 };
 
 const AuthNavigator = createStackNavigator(RouteConfigs, AuthNavigatorConfig);
--- src/values/strings.tsx
+++ src/values/strings.tsx
@@ -2,8 +2,10 @@ const strings = {
     LOGIN: "Log In",
     EMAIL_PLACEHOLDER:"example@example.com",
     PASSWORD_PLACEHOLDER:"password",
+    PASSWORD_RETYPE:"retype password",
     EMAIL_REQUIRED:"Email required",
     PASSWORD_REQUIRED:"Password required",
+    PASSWORD_NOT_MATCHING:"Passwords not the same",
   };
   
   export default strings;
