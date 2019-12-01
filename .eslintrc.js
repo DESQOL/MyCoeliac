4ffa08e4a57950d88569734b7d108dc27725b501
Martijn Vegter <martijn@martijnvegter.com>
2019-12-01 11:53:56 +0100

    chore(style): updated eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,9 +22,20 @@ module.exports = {
         "@typescript-eslint/semi": ["error"],
         "eol-last": ["error", "always"],
         "indent": ["error", 4, { "MemberExpression": 1 }],
+        "multiline-comment-style": ["error", "starred-block"],
+        "no-multiple-empty-lines": ["error", {
+            "max": 1,
+            "maxBOF": 0,
+            "maxEOF": 1
+        }],
         "object-curly-spacing": ["error", "always"],
         "prefer-arrow-callback": "error",
         "quotes": ["error", "single", "avoid-escape"],
         "semi": "off",
+        "spaced-comment": ["error", "always", {
+            "block": {
+                "balanced": true
+            }
+        }],
     },
 };
