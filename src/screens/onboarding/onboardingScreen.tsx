0324545f19bd608ef03ffcd1692ad6c36fd28e05
walorda <walorda@gmail.com>
2019-12-01 10:47:29 +0100

    fixed merge

--- src/screens/onboarding/onboardingScreen.tsx
+++ src/screens/onboarding/onboardingScreen.tsx
@@ -52,21 +52,14 @@ const styles = StyleSheet.create({
         flex: 9,
     },
     viewPager: {
-<<<<<<< HEAD
-        flex: 1
-    }
-
-=======
         flex: 1,
     },
->>>>>>> master
 });
 
 /**
  * Onboarding screen
  */
 class OnboardingScreen extends React.Component<Props, State> {
-<<<<<<< HEAD
 
     viewPager = React.createRef<ViewPagerType>();
 
@@ -153,86 +146,6 @@ class OnboardingScreen extends React.Component<Props, State> {
         }
     }
 
-=======
-  viewPager = React.createRef<ViewPagerType>();
-
-  constructor(props: Props) {
-      super(props);
-      this.pageChanged = this.pageChanged.bind(this);
-      this.handleViewPagerClick = this.handleViewPagerClick.bind(this);
-      this.handleLastPage = this.handleLastPage.bind(this);
-      this.state = {
-          pagePosition: 0,
-      };
-  }
-
-  // Test page's will be replaced later.
-  render() {
-      return (
-          <ViewPager
-              style={styles.viewPager}
-              initialPage={0}
-              ref={this.viewPager}
-              onPageSelected={(EventHandle: any) =>
-                  this.pageChanged(EventHandle.nativeEvent.position)
-              }>
-              <View style={styles.pageStyle} key="1">
-                  <View style={styles.pageContainer}>
-                      <View style={styles.topPart}>
-                          <Text style={styles.HeaderText}> Welcome to {'\n' + name}</Text>
-                      </View>
-                      <Button iconRight title="Button" />
-                      <Text style={styles.skipText} onPress={this.handleViewPagerClick}>
-              Skip
-                      </Text>
-                  </View>
-              </View>
-
-              <View style={styles.pageStyle} key="2">
-                  <View style={styles.pageContainer}>
-                      <View style={styles.topPart}>
-                          <Text style={styles.HeaderText}> Welcome to {'\n' + name}</Text>
-                      </View>
-                      <Button iconRight title="Button" />
-                      <Text style={styles.skipText} onPress={this.handleViewPagerClick}>
-              Skip
-                      </Text>
-                  </View>
-              </View>
-
-              <View style={styles.pageStyle} key="3">
-                  <View style={styles.pageContainer}>
-                      <View style={styles.topPart}>
-                          <Text style={styles.HeaderText}>
-                              {' '}
-                We use these permissions for
-                          </Text>
-                      </View>
-                      <Button title="Done" onPress={this.handleLastPage} />
-                      <Text style={styles.skipText}>Skip</Text>
-                  </View>
-              </View>
-          </ViewPager>
-      );
-  }
-
-  private handleViewPagerClick() {
-    this.viewPager.current!.setPage(this.state.pagePosition + 1);
-  }
-
-  private pageChanged(e: Event) {
-      this.setState({ pagePosition: e as number });
-  }
-
-  async handleLastPage() {
-      try {
-          await AsyncStorage.setItem('onboarding', 'true');
-          this.props.navigation.navigate('Auth');
-      } catch (error) {
-          console.log(error);
-      }
-  }
->>>>>>> master
 }
 
 export default OnboardingScreen;
