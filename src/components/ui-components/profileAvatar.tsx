be8677ef6aab2924b76d6b9d8d8c8d08df30132d
Jordy <jordy.boelhouwer@hva.nl>
2019-12-02 12:23:04 +0100

    [Feature] Added profile avatar component

---
+++ src/components/ui-components/profileAvatar.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Avatar } from 'react-native-elements';
+import { ImageURISource } from 'react-native';
+
+interface ProfileAvatarProps {
+  image: ImageURISource;
+  initials: string;
+}
+
+export default function ProfileAvatar({
+  image,
+  initials,
+}: ProfileAvatarProps): JSX.Element {
+  if (image !== '') {
+    return <Avatar rounded source={image} />;
+  } else {
+    return <Avatar rounded title={initials} />;
+  }
+}
