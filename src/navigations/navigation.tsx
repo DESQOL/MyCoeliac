0d04714ec5808a94d1e1ba5e1bcbfea6f4a78cce
walorda <walorda@gmail.com>
2019-12-16 04:12:57 +0100

    added settings screen

--- src/navigations/navigation.tsx
+++ src/navigations/navigation.tsx
@@ -5,6 +5,7 @@ import AppNavigator from './app-navigation';
 import OnboardingScreen from '../screens/onboarding/onboardingScreen';
 import LoadingScreen from '../screens/loading/loadingScreen';
 import ProfileScreen from '../screens/profile/profileScreen';
+import SettingsScreen from '../screens/settings/settingsScreen';
 
 const RootNavigator = createSwitchNavigator(
     {
@@ -13,9 +14,10 @@ const RootNavigator = createSwitchNavigator(
         Auth: AuthNavigator,
         App: AppNavigator,
         Profile:ProfileScreen,
+        SettingScreen: SettingsScreen
     },
     {
-        initialRouteName: 'App',
+        initialRouteName: 'SettingScreen',
     },
 );
 
---
+++ src/styles/SettingsScreen.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        display: 'flex',
+        flex: 1,
+        flexDirection: 'column',
+    },
+    contentContainer: {
+        flex: 2
+    },
+    emptyContainer: {
+        flex: 3
+    },
+    emptyContainerBottom: {
+        flex: 2
+    },
+    emptyContainerTop: {
+        flex: 1.5
+    }
+});
