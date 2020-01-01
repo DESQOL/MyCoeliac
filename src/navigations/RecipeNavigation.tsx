35c37bfac7cf3df93dbae6d9bfb94657a4b44c0f
mln-i <mln-i@hotmail.com>
2020-01-01 16:19:20 +0100

    refactor name

--- src/navigations/RecipeNavigation.tsx
+++ src/navigations/RecipeNavigation.tsx
@@ -4,10 +4,10 @@ import RecipeScreen from '../screens/recipe/RecipeScreen';
 import RecipeListScreen from '../screens/home/RecipeListScreen';
 
 const RouteConfigs = {
-    HomeScreen: { screen: RecipeListScreen },
+    InitialScreen: { screen: RecipeListScreen },
     RecipeScreen: { screen: RecipeScreen },
 };
 
-const RecipeNavigator = createStackNavigator(RouteConfigs, { initialRouteName: 'HomeScreen', headerMode: 'none' });
+const RecipeNavigator = createStackNavigator(RouteConfigs, { initialRouteName: 'InitialScreen', headerMode: 'none' });
 
 export default RecipeNavigator;
