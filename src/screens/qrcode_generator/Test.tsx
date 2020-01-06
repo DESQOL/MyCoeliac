b25249b1126ee565177e773890790ec6e14b643a
walorda <walorda@gmail.com>
2020-01-06 02:51:47 +0100

    fixed but eslint wont accept

--- src/screens/qrcode_generator/Test.tsx
+++ src/screens/qrcode_generator/Test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
-import QrCodeGenerator from './QrCodegenerator';
+import QrCodeGenerator from './QrcodeGenerator';
 
 export default class Test extends React.Component<{}, {}> {
 
