b6299db9812d053d81e8c92bbd1b521a3543006c
Jordy Boelhouwer <jordyboelhouwer@hotmail.com>
2020-01-09 15:54:30 +0100

    feat: Linked comment page with backend

--- src/components/molecules/CommentList.tsx
+++ src/components/molecules/CommentList.tsx
@@ -1,199 +1,90 @@
 import React, { Component } from 'react';
-import { FlatList, View, Text } from 'react-native';
-import { ListItem } from 'react-native-elements';
-import ProfileAvatar from '../atoms/ProfileAvatar';
-import RateBar from '../atoms/RateBar';
-import PrimaryButton from '../atoms/PrimaryButton';
-import PrimaryTextField from '../atoms/PrimaryTextField';
-import { Rating } from 'react-native-elements';
-
-import styles from '../../styles/components/molecules/CommentList';
+import { View } from 'react-native';
 
 interface CommenListState {
-  input: string;
-  comments: CommentObject[];
-  currentProfile: ProfileObject;
+    input: string;
+    comments: any;
 }
 
 interface CommentListProps {
-  props?: {};
+    recipe: any;
 }
 
 interface ProfileObject {
-  avatar: string;
-  initials: string;
-  recipeRating: number;
+    avatar: string;
+    initials: string;
+    recipeRating: number;
 }
 
 interface CommentObject {
-  id: number;
-  body: string;
-  profile: ProfileObject;
+    id: number;
+    comment: string;
 }
 
-const testProfile1: ProfileObject = {
-    avatar:
-    'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-    initials: 'JB',
-    recipeRating: 1,
-};
-
-const testProfile2: ProfileObject = {
-    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-    initials: 'ML',
-    recipeRating: 5,
-};
-
-const comments = [
-    {
-        id: 1,
-        profile: testProfile1,
-        body: 'Wat een smerig gerecht!',
-    },
-    {
-        id: 2,
-        profile: testProfile2,
-        body: 'Wat zeg jij nou! Het is heerlijk!',
-    },
-    {
-        id: 3,
-        profile: testProfile1,
-        body: 'Jij hebt gewoon geen smaak!',
-    },
-    {
-        id: 4,
-        profile: testProfile2,
-        body:
-      'Je weet niet waar je het over hebt man! Als je zo doorgaat sla ik je neer!',
-    },
-    {
-        id: 5,
-        profile: testProfile1,
-        body: 'Waar heb je het nou over vrouw! Denk je dat je mij aan kan ofzo!',
-    },
-    {
-        id: 6,
-        profile: testProfile2,
-        body: 'Kom naar de stad dan! Dan ga je zien wat er met je gebeurt',
-    },
-    {
-        id: 7,
-        profile: testProfile1,
-        body: 'Okee, morgen 3 uur in de stad. Douw ik even wat Gluten in je porem!',
-    },
-    {
-        id: 8,
-        profile: testProfile2,
-        body: 'Afgesproken! Neem ik een stokbrood mee!',
-    },
-    {
-        id: 9,
-        profile: testProfile1,
-        body: 'Ik ook! Vanavond wordt je laatste maaltijd!',
-    },
-    {
-        id: 10,
-        profile: testProfile2,
-        body: 'Kijk er nu al naar uit!',
-    },
-];
-
-const currentProfile = testProfile1;
-
 export default class CommentList extends Component<
-  CommentListProps,
-  CommenListState
-> {
+    CommentListProps,
+    CommenListState
+    > {
     constructor(props: CommentListProps) {
         super(props);
     }
 
-  public readonly state: CommenListState = {
-      input: '',
-      currentProfile: testProfile1,
-      comments: comments,
-  };
+    public readonly state: CommenListState = {
+        input: '',
+        comments: {}
+    };
 
-  updateInput(input: string): void {
-      this.setState({
-          input: input,
-      });
-  }
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    fetchComments = async () => {
+        let responseJson: any;
+        try {
+            const response = await fetch('https://desqol.hihva.nl/recipe/195529/comments', {
+                method: 'GET',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+            });
 
-  addNewComment(): void {
-      console.log('Button clicked');
-      const newComment: CommentObject = {
-          id: this.state.comments.length + 1,
-          profile: currentProfile,
-          body: this.state.input,
-      };
-      this.state.comments.push(newComment);
-      this.setState({
-          input: '',
-      });
-  }
+            responseJson = await response.json();
+            console.log(responseJson);
+            this.setState({ comments: responseJson });
+        } catch (error) {
+            console.log(error);
+            return;
+        }
+    };
 
-  render(): JSX.Element {
-      return (
-          <View>
-              <FlatList
-                  data={comments}
-                  removeClippedSubviews={false}
-                  renderItem={({ item }: { item: CommentObject }) => (
-                      <View>
-                          <ListItem
-                              title={item.body}
-                              subtitle={
-                                  <View style={styles.rating}>
-                                      <Rating
-                                          imageSize={20}
-                                          readonly
-                                          startingValue={item.profile.recipeRating}
-                                      />
-                                  </View>
-                              }
-                              leftAvatar={
-                                  <ProfileAvatar
-                                      image={item.profile.avatar}
-                                      initials={item.profile.initials}
-                                  />
-                              }
-                          />
-                      </View>
-                  )}
-                  keyExtractor={item => item.id.toString()}
-                  ListHeaderComponent={
-                      <View>
-                          <Text style={styles.title}>Comments</Text>
-                      </View>
-                  }
-                  ListFooterComponent={
-                      <View>
-                          <RateBar />
-                          <View style={styles.newComment}>
-                              <ProfileAvatar
-                                  image={currentProfile.avatar}
-                                  initials={currentProfile.initials}
-                                  avatarStyle={styles.avatar}
-                              />
-                              <View>
-                                  <PrimaryTextField
-                                      onChangeText={input => this.updateInput(input)}
-                                      textFieldStyle={styles.input}
-                                      value={this.state.input}
-                                  />
-                              </View>
-                              <PrimaryButton
-                                  title="Enter"
-                                  type="solid"
-                                  componentStyle={styles.primaryButton}
-                                  onClick={() => this.addNewComment()}
-                              />
-                          </View>
-                      </View>
-                  }
-              />
-          </View>
-      );
-  }
+    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+    handleCommentSubmit = async () => {
+        console.log('Button clicked');
+        let responseJson: any;
+        try {
+            const response = await fetch('https://desqol.hihva.nl/recipe/195529/comments', {
+                method: 'GET',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    comment: this.state.input
+                })
+            });
+            responseJson = await response.json();
+            console.log(responseJson);
+        } catch (error) {
+            console.error(error);
+            return;
+        }
+        this.setState({
+            input: '',
+        });
+    };
+
+    render(): JSX.Element {
+        return (
+            <View>
+            </View>
+        );
+    }
 }
--- src/styles/screens/CommentScreen.tsx
+++ src/styles/screens/CommentScreen.tsx
@@ -1,7 +1,40 @@
 import { StyleSheet } from 'react-native';
+import { PrimaryGray } from '../config/Colors';
 
 export default StyleSheet.create({
+    avatar: {
+        justifyContent: 'flex-start',
+    },
     commentList: {
         flex: 1,
+    },
+    input: {
+        borderColor: PrimaryGray,
+        borderWidth: 1,
+        height: 40,
+        width: 180,
+    },
+    loading: {
+        flex: 1,
+        padding: 20
+    },
+    newComment: {
+        display: 'flex',
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginBottom: 10,
+        marginTop: 20,
+    },
+    primaryButton: {
+        width: 100,
+    },
+    rating: {
+        marginRight: 40,
+        marginTop: 10,
+    },
+    title: {
+        fontSize: 25,
+        textAlign: 'center',
     },
 });
