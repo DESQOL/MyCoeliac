0979b795cd773f676138a59d2509ae0d38e95989
Martijn Vegter <martijn@martijnvegter.com>
2020-01-19 21:42:49 +0100

    chore(style): extend from react/recommended instead of react/all

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
         'plugin:react/recommended',
         'plugin:jsx-a11y/recommended',
-        'plugin:react/all',
+        'plugin:react/recommended',
         'plugin:react-native/all',
     ],
     plugins: [
